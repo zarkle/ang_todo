4bd2984b14aa5c3afa6c80f765a2ef4b9007a9a8
Beverly Pham <beverly.pham@gmail.com>
2018-10-20 12:03:15 -0700

    add todo item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonUnitComponent
+    InputButtonUnitComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
