17b09db0e72e34d206d179daf305f58c3565084a
Beverly Pham <beverly.pham@gmail.com>
2018-10-20 10:29:39 -0700

    add input button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputButtonUnitComponent
   ],
   imports: [
     BrowserModule,
